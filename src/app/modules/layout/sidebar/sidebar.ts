7f1924a5f1a3b10821dbe506dd971ae0fd7bb127
pkbrat424 <pkbrat424@gmail.com>
2025-12-26 13:51:04 +0530

    Added bitbucket

--- src/app/modules/layout/sidebar/sidebar.ts
+++ src/app/modules/layout/sidebar/sidebar.ts
@@ -11,6 +11,7 @@ export class Sidebar {
   socialLinks = [
     { name: 'PDF/Resume', icon: 'fa-regular fa-file-pdf', url: 'https://drive.google.com/file/d/17nte5pqqvtu9YQKlgpZrb0098hSFWxzi/view?usp=sharing' },
     { name: 'GitHub', icon: 'fa-brands fa-github', url: 'https://github.com/Ipeeka' },
+    { name: 'Bitbucket', icon: 'fa-brands fa-bitbucket', url: 'https://bitbucket.org/pkbrat424_/workspace/overview/' },
     { name: 'LinkedIn', icon: 'fa-brands fa-linkedin', url: 'https://www.linkedin.com/in/pawan-kumar-223b3a220/' },
     // { name: 'Instagram', icon: 'fa-brands fa-instagram', url: 'https://instagram.com/yourusername' }
   ];
