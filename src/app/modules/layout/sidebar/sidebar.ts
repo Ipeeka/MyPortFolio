0f3ef1df0009806e3aad8976c44bf667cddd4f8d
pkbrat424 <pkbrat424@gmail.com>
2025-12-26 13:32:23 +0530

    Added resume link

--- src/app/modules/layout/sidebar/sidebar.ts
+++ src/app/modules/layout/sidebar/sidebar.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 })
 export class Sidebar {
   socialLinks = [
-    { name: 'PDF/Resume', icon: 'fa-regular fa-file-pdf', url: '#' },
+    { name: 'PDF/Resume', icon: 'fa-regular fa-file-pdf', url: 'https://drive.google.com/file/d/17nte5pqqvtu9YQKlgpZrb0098hSFWxzi/view?usp=sharing' },
     { name: 'GitHub', icon: 'fa-brands fa-github', url: 'https://github.com/Ipeeka' },
     { name: 'LinkedIn', icon: 'fa-brands fa-linkedin', url: 'https://www.linkedin.com/in/pawan-kumar-223b3a220/' },
     // { name: 'Instagram', icon: 'fa-brands fa-instagram', url: 'https://instagram.com/yourusername' }
